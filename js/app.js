f572ef7eb8fd46c8def58cfd650748e4f53f077e
Anton Myzenkov <amyzenkov@gmail.com>
2016-09-27 21:29:13 +0300

    study of angularjs

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+/* jshint unused: false */
+/* global angular */
+
+"use strict";
+
 var movieStubApp = angular.module('movieStubApp', ['ngResource', 'ngRoute']);
+movieStubApp.config(['$compileProvider', function($compileProvider) {
+	$compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension):/);
+}]);
--- js/service/services.js
+++ js/service/services.js
@@ -1,3 +1,14 @@
+/* jshint node: true */
+/* jshint browser: true */
+/* jshint esversion: 6 */
+/* global movieStubApp */
+
+"use strict";
+
 movieStubApp.factory('movieStubFactory', function($resource) {
 	return $resource('http://moviestub.cloudno.de/movies');
+});
+
+movieStubApp.factory('movieStubBookingsFactory', function($resource) {
+	return $resource('http://moviestub.cloudno.de/bookings');
 });
